07380537ad9718fa5899b75fda16b74796019461
Theppitak M <141404845+mmmmmob@users.noreply.github.com>
2024-05-19 13:56:37 +0700

    Initial Commit

---
+++ Lotto Journal/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Lotto Journal
+//
+//  Created by Theppitak M. on 19.05.2024.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ Lotto Journal/Lotto_JournalApp.swift
@@ -0,0 +1,32 @@
+//
+//  Lotto_JournalApp.swift
+//  Lotto Journal
+//
+//  Created by Theppitak M. on 19.05.2024.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Lotto_JournalApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
