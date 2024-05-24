9c33d90ad63562d2d71077293e36abb925cf3c0c
Theppitak M <141404845+mmmmmob@users.noreply.github.com>
2024-05-24 14:16:09 +0700

    chore: add new files for MVVM in MyLottoView

--- Lotto Journal/Models/MyLottery.swift
+++ Lotto Journal/Models/MyLottery.swift
@@ -9,7 +9,7 @@ import Foundation
 import SwiftData
 
 @Model
-final class MyLotto {
+final class MyLottery {
     var timestamp: Date
     
     init(timestamp: Date) {
---
+++ Lotto Journal/ViewModels/MyLotteryViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  MyLotteryViewModel.swift
+//  Lotto Journal
+//
+//  Created by Theppitak M. on 24.05.2024.
+//
+
+import Foundation
