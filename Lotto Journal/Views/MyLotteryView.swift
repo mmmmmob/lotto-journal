fee3ecf48c4e9966293b85e377b6dac99cf60c77
Theppitak M <141404845+mmmmmob@users.noreply.github.com>
2024-05-27 21:19:36 +0700

    feat: update ui and add delete with SwiftData

--- Lotto Journal/Views/MyLotteryView.swift
+++ Lotto Journal/Views/MyLotteryView.swift
@@ -10,41 +10,66 @@ import SwiftData
 
 struct MyLotteryView: View {
     
-    @Query(sort: \Lottery.amount) private var lotteries: [Lottery]
+    @Query(sort: \Lottery.amount, order: .reverse) private var lotteries: [Lottery]
+    @Environment(\.modelContext) private var modelContext
     @State var isAddding: Bool = false
+    @State var searchNumber: String = ""
     
     var body: some View {
         NavigationStack {
-            List {
-                ForEach(lotteries) { lottery in
-                    HStack {
-                        HStack {
-                            Text(lottery.number)
-                                .font(.system(.headline, design: .monospaced, weight: .semibold))
-                                .tracking(7)
-                            if lottery.amount > 1 {
-                                Text("x \(lottery.amount)")
-                                    .font(.system(.caption, weight: .thin))
-                                    .foregroundStyle(.secondary)
+            Group {
+                if lotteries.isEmpty {
+                    ContentUnavailableView("Enter your first lottery", systemImage: "wand.and.stars")
+                } else {
+                    List {
+                        ForEach(lotteries) { lottery in
+                            HStack {
+                                HStack {
+                                    Text(lottery.number)
+                                        .font(.system(.headline, design: .monospaced, weight: .semibold))
+                                        .tracking(7)
+                                    if lottery.amount > 1 {
+                                        Text("x \(lottery.amount)")
+                                            .font(.system(.caption, weight: .thin))
+                                            .foregroundStyle(.secondary)
+                                    }
+                                }
+                                Spacer()
+                                HStack(spacing: 5) {
+                                    lottery.tagSymbol
+                                        .foregroundStyle(lottery.tagColor)
+                                        .imageScale(.small)
+                                    Text(lottery.status.description)
+                                        .font(.system(.subheadline, weight: .light))
+                                }
                             }
                         }
-                        Spacer()
-                        HStack(spacing: 5) {
-                            lottery.tagSymbol
-                                .foregroundStyle(lottery.tagColor)
-                            Text(lottery.status.description)
-                                .font(.system(.subheadline, weight: .light))
-                        }
+                        .onDelete(perform: { indexSet in
+                            indexSet.forEach { index in
+                                let lottery = lotteries[index]
+                                modelContext.delete(lottery)
+                            }
+                        })
                     }
+                    .listStyle(.plain)
                 }
             }
+            .searchable(text: $searchNumber)
+            .keyboardType(.numberPad)
             .navigationTitle("My Lotter\(lotteries.count > 1 ? "ies" : "y")")
             .toolbar {
-                Button(action: {
-                    isAddding.toggle()
-                }, label: {
-                        Image(systemName: "plus")
-                })
+                if !lotteries.isEmpty {
+                    ToolbarItem(placement: .topBarLeading) {
+                        EditButton()
+                    }
+                }
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button(action: {
+                        isAddding.toggle()
+                    }, label: {
+                        Image(systemName: "plus.circle.fill")
+                    })
+                }
             }
             .sheet(isPresented: $isAddding) {
                 AddMyLotteryView(amountBought: 1)
